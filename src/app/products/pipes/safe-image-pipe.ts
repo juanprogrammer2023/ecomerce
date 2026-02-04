c321cb63388609a41995d035ab15213396156093
Juan Pablo Pineda Madrigal <juanpp282@gmail.com>
2026-02-03 23:58:42 -0500

    reject

--- src/app/products/pipes/safe-image-pipe.ts
+++ src/app/products/pipes/safe-image-pipe.ts
@@ -1,23 +1,13 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
-import { catchError, map } from 'rxjs/operators';
 
 @Pipe({
   name: 'safeImage',
-  standalone: true
 })
 export class SafeImagePipe implements PipeTransform {
-  constructor(private http: HttpClient) {}
 
-  transform(url: string | undefined, defaultImage: string = '/images/no-image.png'): Observable<string> {
-    if (!url) {
-      return of(defaultImage);
-    }
-
-    return this.http.head(url, { observe: 'response' }).pipe(
-      map(response => response.ok ? url : defaultImage),
-      catchError(() => of(defaultImage))
-    );
+  transform(url: string | undefined, defaultImage: string = '/images/no-image.png'): string {
+    console.log(url)
+    return url || defaultImage;
   }
+
 }
